750a837b0bbc14bf21977490c2dd30d53025f2b2
hj-66 <a01088200534@gmail.com>
2026-04-23 10:13:32 +0900

    중복 괄호 제외 전체 해결(수정중)

--- TDDcalc-2/src/main/java/com/back/Calc.java
+++ TDDcalc-2/src/main/java/com/back/Calc.java
@@ -1,26 +1,58 @@
 package com.back;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Calc {
     public static int run(String content) {
         String[] str = content.split(" ");
-        return search(str, "+");
+        String [] new_str = FirstSearch(str);
+
+        return search(new_str, "+");
+    }
+
+    private static String[] FirstSearch(String[] str) {
+        List<String> new_str = new ArrayList<>();
+        // 괄호 먼저 계산
+        int start = -1;
+        int end;
+        boolean isFirst = false;
+        boolean isMinus = false;
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].contains("(")) { // 괄호 시작
+                if (str[i].contains("-")) isMinus = true;
+                start = i;
+                isFirst = true;
+            } else if (str[i].contains(")")) {
+                end = i;
+                new_str.add((isMinus ? "-" : "") + search(Arrays.copyOfRange(str, start, end + 1), "+"));
+                start = -1;
+                isFirst = false;
+                isMinus = false;
+            } else {
+                if (!isFirst) new_str.add(str[i]);
+            }
+        }
+
+        return new_str.toArray(new String[0]);
     }
 
     private static int search(String[] str, String op) {
-        int sum = Integer.parseInt(str[0]) * (op.equals("-") ? -1 : 1);
         String b = "+"; // 연산자
+        int sum = Integer.parseInt(filter(str[0])) * (op.equals("-") ? -1 : 1);
 
         for (int i = 1; i < str.length; i++) {
-            if (str[i].matches("[+\\-*]")) {
-                b = str[i];
-            } else if (str[i].matches("-?\\d+")) {
+            String s = filter(str[i]);
+            if (s.matches("[+\\-*]")) {
+                b = s;
+            } else if (s.matches("-?\\d+")) {
                 if (b.equals("+") || b.equals("-")) {
                     sum = calculate(sum, "+", search(Arrays.copyOfRange(str, i, str.length), b));
                     break;
                 } else if (b.equals("*")) {
-                    sum = calculate(sum, b, Integer.parseInt(str[i]));
+                    sum = calculate(sum, b, Integer.parseInt(s));
                 }
             }
         }
@@ -28,10 +60,15 @@ public class Calc {
         return sum;
     }
 
+    private static String filter(String s) {
+        if (s.contains("(")) s = s.replace("-", "");
+        s = s.replaceAll("[()]", "");
+        return s;
+    }
+
     private static int calculate(int a, String b, int c) {
         return switch (b) {
             case "+" -> a + c;
-            case "-" -> a - c;
             case "*" -> a * c;
             default -> a;
         };
